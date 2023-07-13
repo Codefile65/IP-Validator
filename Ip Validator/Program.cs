4a53d4a2fddfb1ac182c50fd334b6eb72f94f432
KWISADMIN <KWISADMIN@DESKTOP-N8LEKSG>
2023-07-13 12:39:19 +0700

    Add project files.

---
+++ Ip Validator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ip_Validator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
